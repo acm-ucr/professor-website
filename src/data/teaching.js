b50fe3435dbec03171ec660be6ea762d7fbcc81a
Divyank Shah <divyank.shah.2016@gmail.com>
2023-06-25 03:32:48 -0400

    teaching basically done

---
+++ src/data/teaching.js
@@ -0,0 +1,16 @@
+const Teaching = {
+  "2021-2022": {
+    Fall: ["CS178A", "CS10C"],
+    Winter: ["CS178A", "CS10C"],
+    Spring: ["CS178A", "CS10C"],
+    Summer: ["CS178A", "CS10C"],
+  },
+  "2022-2023": {
+    Fall: ["CS178A", "CS10C"],
+    Winter: ["CS178A", "CS10C"],
+    Spring: ["CS178A", "CS10C"],
+    Summer: ["CS178A", "CS10C"],
+  },
+};
+
+export default Teaching;
