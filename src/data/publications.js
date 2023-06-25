eb915dacf86cb20c65f0b0c8ba4dc4e406a51c28
menthy-wu <wumengxuan0728@gmail.com>
2023-06-25 00:33:49 -0700

    mobile publication

--- src/data/publications.js
+++ src/data/publications.js
@@ -19,12 +19,15 @@ const Publications = {
   2022: [
     {
       title: "THIS IS A PAPER TITLE",
-      description: "THIS IS A PAPER DESCRIPTION",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus mattis rhoncus urna neque viverra. Non curabitur gravida arcu ac tortor dignissim convallis aenean et. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Proin fermentum leo vel orci porta non. Odio ut enim blandit volutpat. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Nulla facilisi morb",
       link: "THIS IS A PAPER LINK",
     },
     {
       title: "THIS IS A PAPER TITLE",
-      description: "THIS IS A PAPER DESCRIPTION",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus mattis rhoncus urna neque viverra. Non curabitur gravida arcu ac tortor dignissim convallis aenean et. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Proin fermentum leo vel orci porta non. Odio ut enim blandit volutpat. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Nulla facilisi morb",
+
       link: "THIS IS A PAPER LINK",
     },
   ],
