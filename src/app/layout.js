bbc945ae29f72e10f22f1a5782c1f107ea44aee9
Divyank Shah <divyank.shah.2016@gmail.com>
2023-06-25 01:40:48 -0400

    add tailwind colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 /* eslint-disable new-cap */
+import Navigation from "@/components/Navigation";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,81 @@
+"use client";
+
 import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <Navbar
+      collapseOnSelect
+      className="py-0 font-lexend w-full px-3 m-0 bg-ula-blue flex justify-between items-center"
+      expand="md"
+      fixed="top"
+    >
+      <Navbar.Brand className="p-0">
+        <Link
+          eventkey="1"
+          className="p-0 text-white no-underline font-medium text-2xl"
+          href="/"
+        >
+          Undergraduate Learning Assistants
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav" className="!m-0 !p-0">
+        <Nav className=" no-underline ml-auto text-2xl mb-0">
+          <Nav.Link
+            as={Link}
+            eventkey="6"
+            href="/"
+            className={`${
+              usePathname() === "/" ? "!bg-ula-yellow text-black" : "text-white"
+            } mb-0 py-1 px-3 no-underline ml-auto !text-xl whitespace-nowrap w-full text-center hover:cursor-pointer`}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            eventkey="6"
+            href="/overview"
+            className={`${
+              usePathname() === "/overview"
+                ? "!bg-ula-yellow text-black"
+                : "text-white"
+            } mb-0 py-1 px-3 no-underline ml-auto !text-xl whitespace-nowrap w-full text-center hover:cursor-pointer`}
+          >
+            Overview
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            eventkey="6"
+            href="/calendars"
+            className={`${
+              usePathname() === "/calendars"
+                ? "!bg-ula-yellow text-black"
+                : "text-white"
+            } mb-0 py-1 px-3 no-underline ml-auto !text-xl whitespace-nowrap w-full text-center hover:cursor-pointer`}
+          >
+            Calendars
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            eventkey="6"
+            href="/ulas"
+            className={`${
+              usePathname() === "/ulas"
+                ? "!bg-ula-yellow text-black"
+                : "text-white"
+            } mb-0 py-1 px-3 no-underline ml-auto !text-xl whitespace-nowrap w-full text-center hover:cursor-pointer`}
+          >
+            ULAs
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navigation;
