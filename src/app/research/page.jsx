e575cd187eb975dcaf6fa11331e3248e2ff2d9a5
menthy-wu <wumengxuan0728@gmail.com>
2023-06-25 00:25:57 -0700

    reasearch mobile

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -9,15 +9,15 @@ const Page = () => {
       <Tag title="Research" />
       {Research.map((research, index) => (
         <div className="my-4 w-full" key={index}>
-          <p className="text-professor-blue border-b-4 border-professor-yellow w-fit text-xl">
+          <p className="text-professor-blue border-b-4 border-professor-yellow w-fit text-lg md:text-xl">
             {research.title}
           </p>
-          <div className="flex justify-start items-stretch w-full">
+          <div className="flex justify-start items-center md:items-stretch w-full flex-col md:flex-row">
             {research.image && (
-              <img className="w-3/12 mr-4" src={research.image} />
+              <img className="w-1/2 md:w-3/12 m-4" src={research.image} />
             )}
             <div className="flex justify-between content-between flex-col ">
-              <p className="text-lg">{research.description}</p>
+              <p className="text-base md:text-lg m-4">{research.description}</p>
               <div className="w-full flex justify-end">
                 {research.link && (
                   <a
