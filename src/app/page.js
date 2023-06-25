7ecd6617a25f5a28967a8300c4eca1f0ab1b0061
menthy-wu <wumengxuan0728@gmail.com>
2023-06-25 00:17:22 -0700

    home mobile

--- src/app/page.js
+++ src/app/page.js
@@ -10,23 +10,27 @@ const Home = () => {
         className="h-36 w-36 object-cover rounded-full border-8 border-white -mt-20"
         src="/profile.png"
       />
-      <p className="text-professor-blue font-bold text-3xl p-0 m-0">
+      <p className="w-9/12 text-center text-lg text-professor-blue font-bold md:text-3xl p-0 m-0">
         {home.name}
       </p>
-      <p className="text-professor-gray text-xl p-0 m-0">{home.title}</p>
-      <p className="text-professor-gray text-xl p-0 border-b-4 border-professor-yellow pb-2 mb-2">
+      <p className="w-9/12 text-center text-xs text-professor-gray md:text-xl p-0 m-0">
+        {home.title}
+      </p>
+      <p className="w-9/12 text-center text-xs text-professor-gray md:text-xl p-0 border-b-4 border-professor-yellow pb-2 mb-2">
         {home.position}
       </p>
-      <p className="text-professor-gray text-lg p-0 m-0">{home.major}</p>
+      <p className="w-9/12 text-center text-sm text-professor-gray md:text-lg p-0 m-0">
+        {home.major}
+      </p>
 
-      <p className="text-professor-blue font-bold text-base p-0 m-0">
+      <p className="w-9/12 text-center text-sm text-professor-blue font-bold md:text-base p-0 m-0">
         {home.email} | {home.phone}
       </p>
-      <p className="text-professor-blue font-bold text-base p-0 mb-8">
+      <p className="w-9/12 text-center text-sm text-professor-blue font-bold md:text-base p-0 mb-8">
         {home.office}
       </p>
       {home.descriptions.map((description, index) => (
-        <p key={index} className="text-black text-lg p-0 m-4 w-9/12">
+        <p key={index} className="text-black text-sm md:text-lg p-0 m-4 w-9/12">
           {description}
         </p>
       ))}
