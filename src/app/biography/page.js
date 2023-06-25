cfe3f507bcb3f29c24342d6c9447b639e399a37d
menthy-wu <wumengxuan0728@gmail.com>
2023-06-25 00:20:39 -0700

    bio mobile

--- src/app/biography/page.js
+++ src/app/biography/page.js
@@ -3,21 +3,23 @@ import biography from "@/data/biography";
 import Tag from "@/components/Tag";
 const page = () => {
   return (
-    <div className="w-full flex flex-col items-center">
-      <div className="w-10/12 flex justify-start m-8">
+    <div className="w-full flex flex-col items-center my-8">
+      <div className="w-10/12 flex justify-start ">
         <Tag title="Biography" />
       </div>
       {biography.map((paragraph, index) => (
         <div
           key={index}
-          className={`flex w-10/12 items-center justify-center ${
-            index % 2 == 0 ? "flex-row" : "flex-row-reverse"
+          className={`flex w-10/12 items-center justify-center flex-col ${
+            index % 2 == 0 ? "md:flex-row" : "md:flex-row-reverse"
           }`}
         >
           {paragraph.photopath != "" && (
-            <img className="w-1/4 m-3" src={paragraph.photopath} />
+            <img className="w-1/2 md:w-1/4 m-3" src={paragraph.photopath} />
           )}
-          <p className="text-black text-lg p-0 m-3">{paragraph.text}</p>
+          <p className="text-black text-sm md:text-lg p-0 m-3">
+            {paragraph.text}
+          </p>
         </div>
       ))}
     </div>
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Tag = ({ title }) => {
   return (
-    <div className="border-l-8 border-professor-yellow bg-professor-blue px-6 py-2 w-fit text-white text-2xl font-medium">
+    <div className="border-l-8 border-professor-yellow bg-professor-blue px-6 py-2 w-fit text-white text-lg md:text-2xl font-medium">
       {title}
     </div>
   );
